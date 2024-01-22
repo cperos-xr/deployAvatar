cb39c73ef249aed5c9ddf6de2de927a7863e09c5
Constantine <cperos.xr@gmail.com>
2024-01-21 23:01:50 -1000

    Reinitialized repository and added existing files

---
+++ src/components/Experience.jsx
@@ -0,0 +1,31 @@
+import { RigidBody } from "@react-three/rapier";
+import { Model} from "./Small_villa";
+import { Dome } from "./Sky_dome";
+import { CharacterController } from "./CharacterController/CharacterController";
+import { Manager } from "../hooks/useYuka";
+import { JacobNpc } from "./Npcs";
+import { YukaMesh } from "./Yuka/YukaMesh";
+
+export const Experience = ({ heroRef }) => {
+  return (
+    <>
+      <ambientLight intensity={2} />
+      <directionalLight position={[0, 20, 20]} intensity={1} />
+      {/* YUKA manager */}
+      <Manager>
+        {/* Hero character controller component */}
+        <CharacterController reference={heroRef} />
+        {/* Convai NPC Jacob component */}
+        <JacobNpc name="Jacob" position={[0, 0, 8]} heroRef={heroRef} />
+        {/* <YukaMesh /> */}
+        <group>
+          {/* Rapier triangler collider for stage */}
+          <RigidBody friction={2} colliders="trimesh" type="fixed">
+            <Model />
+            <Dome position={[10, -.6, 0]}/>
+          </RigidBody>
+        </group>
+      </Manager>
+    </>
+  );
+};
